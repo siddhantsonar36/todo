f1cd93a10933a5a5a3f87777dda79f674385923a
siddhantsonar36 <siddhantsonar36@gmail.com>
2022-10-17 12:50:55 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import Connection from './database/db.js';
+import cors from 'cors';
+import route from './routes/route.js';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(cors());
+
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', route);
 
 const PORT = 8000;
 
