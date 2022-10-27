729c21c5a7ee3aa0b26421ac19f8318cd4f5aa1c
siddhantsonar36 <siddhantsonar36@gmail.com>
2022-10-27 13:12:15 +0530

    updated the code

--- server/controller/todo-controller.js
+++ server/controller/todo-controller.js
@@ -1,18 +1,68 @@
-import todo from "../model/Todo.js";
+import todo from '../model/Todo.js';
 
 export const addTodo = async (request, response) => {
-    try {
+  try {
+    const newTodo = await todo.create({
+      data: request.body.data,
+      createdAt: Date.now(),
+    });
 
-        const newTodo = await todo.create({
-            data: request.body.data,
-            createdAt: Date.now()
-        })
-    
-        await newTodo.save();
-    
-        return response.status(200).json(newTodo);
-        
-    } catch (error) {
-        return response.status(500).json(error.message)
-    }
+    await newTodo.save();
+
+    return response.status(200).json(newTodo);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+};
+
+export const getTodos = async (request, response) => {
+  try {
+    const allTodos = await todo.find({}).sort({ createdAt: -1 });
+
+    return response.status(200).json(allTodos);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+};
+
+export const toggleTodoDone = async (request, response) => {
+  try {
+    const todoRef = await todo.findById(request.params.id);
+
+    const updatedTodo = await todo.findOneAndUpdate(
+      { _id: request.params.id },
+      { done: !todoRef.done }
+    );
+
+    await updatedTodo.save();
+
+    return response.status(200).json(updatedTodo);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+};
+
+export const updateTodo = async (request, response) => {
+  try {
+    await todo.findOneAndUpdate(
+      { _id: request.params.id },
+      { data: request.body.data }
+    );
+
+    const updatedTodo = await todo.findById(request.params.id);
+
+    return response.status(200).json(updatedTodo);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
+};
+
+export const deleteTodo = async (request, response) => {
+  try {
+    const updatedTodo = await todo.findByIdAndDelete(request.params.id);
+
+    return response.status(200).json(updatedTodo);
+  } catch (error) {
+    return response.status(500).json(error.message);
+  }
 };
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,18 @@
-import  express, { Router }  from "express";
-import { addTodo } from '../controller/todo-controller.js'
+import express, { Router } from 'express';
+import {
+  addTodo,
+  deleteTodo,
+  getTodos,
+  toggleTodoDone,
+  updateTodo,
+} from '../controller/todo-controller.js';
 
 const route = express.Router();
 
-route.post('/todos', addTodo)
+route.post('/todos', addTodo);
+route.get('/todos', getTodos);
+route.get('/todos/:id', toggleTodoDone);
+route.put('/todos/:id', updateTodo);
+route.delete('/todos/:id', deleteTodo);
 
 export default route;
